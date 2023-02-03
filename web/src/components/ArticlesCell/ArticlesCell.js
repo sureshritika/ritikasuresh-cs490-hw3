bedc3ecef014fe30a3bc8416c6b9c82db8ab5c0f
sureshritika <ritikasuresh19@gmail.com>
2023-02-02 21:36:28 -0500

    final commit

--- web/src/components/ArticlesCell/ArticlesCell.js
+++ web/src/components/ArticlesCell/ArticlesCell.js
@@ -7,6 +7,9 @@ export const QUERY = gql`
       title
       body
       createdAt
+      user {
+        name
+      }
     }
   }
 `
@@ -21,7 +24,7 @@ export const Success = ({ articles }) => {
   return (
     <div className="space-y-10">
       {articles.map((article) => (
-        <Article article={article} key={article.id} />
+        <Article article={article} key={article.id} summary={true} />
       ))}
     </div>
   )
